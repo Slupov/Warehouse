0b67234a011eea7c5f46be88a7116d93778c5cb3
Slupov <stoyan.lupov@gmail.com>
2019-09-07 21:09:48 +0300

    [Delete views are now implemented using reflection]

---
+++ Warehouse.Web/Views/Shared/Components/GenericDeleteViewViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Web.Models.Components;
+
+namespace Warehouse.Web.Views.Shared.Components
+{
+    public class GenericDeleteViewViewComponent : ViewComponent
+    {
+        public GenericDeleteViewViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(GenericViewObjectModel vm)
+        {
+            var result = (IViewComponentResult)View("GenericDeleteView", vm);
+            return result;
+        }
+    }
+}
