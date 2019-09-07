3f33b6c4fde11fc483cfe5684d3d970d77ca0742
Slupov <stoyan.lupov@gmail.com>
2019-09-07 21:30:00 +0300

    [Started work on generic Create views]

---
+++ Warehouse.Web/Views/Shared/Components/GenericCreateViewViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Web.Models.Components;
+
+namespace Warehouse.Web.Views.Shared.Components
+{
+    public class GenericCreateViewViewComponent : ViewComponent
+    {
+        public GenericCreateViewViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(GenericViewObjectModel vm)
+        {
+            var result = (IViewComponentResult)View("GenericCreateView", vm);
+            return result;
+        }
+    }
+}
